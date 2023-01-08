eee7cbcfeb3776a57cce396bfcac6bb9e127707b
turnPenny <bohdan.miroshnik23@gmail.com>
2023-01-08 19:29:00 +0100

    SingleEvent component is created

--- pages/events/[category]/[id].jsx
+++ pages/events/[category]/[id].jsx
@@ -1,9 +1,39 @@
-const EventPage = () => {
+import Image from 'next/image';
+
+const EventPage = ({ data }) => {
+  console.log(data);
   return (
     <div>
-      <h2> single event</h2>
+      <Image src={data.image} alt={data.title} width={400} height={300} />
+      <h2>{data.title}</h2>
+      <p>{data.description}</p>
     </div>
   );
 };
 
 export default EventPage;
+
+export async function getStaticPaths() {
+  const { allEvents } = await import('../../../data/data.json');
+  const allPaths = allEvents.map(({ id, city }) => {
+    return {
+      params: {
+        category: city,
+        id,
+      },
+    };
+  });
+
+  return {
+    paths: allPaths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { allEvents } = await import('./../../../data/data.json');
+
+  return {
+    props: { data: allEvents.find((event) => context.params.id == event.id) },
+  };
+}
